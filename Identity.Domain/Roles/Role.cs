a37e846eb77f8f006e85a9623286658633f1fb8e
Reiysher <reiysher25@gmail.com>
2023-07-01 23:13:50 +0300

    feat(roles): add roles

---
+++ Identity.Domain/Roles/Role.cs
@@ -0,0 +1,44 @@
+﻿namespace Identity.Domain.Roles;
+
+public sealed class Role : Entity, IAggregateRoot
+{
+    private string _id = default!;
+    private string? _name;
+    private string? _description;
+    private readonly HashSet<UserRole>? _users;
+    private readonly HashSet<RoleClaim>? _claims;
+
+    public string Id => _id;
+
+    public string? Name => _name;
+
+    public string? Description => _description;
+
+    public IReadOnlyCollection<UserRole>? Users => _users?.ToList();
+
+    public IReadOnlyCollection<RoleClaim>? Claims => _claims?.ToList();
+
+    private Role() { }
+
+    private Role(string name, string? description = null)
+    {
+        if (String.IsNullOrWhiteSpace(name))
+        {
+            throw new ValidationException(Messages.Exceptions.InvalidRoleName);
+        }
+
+        _id = Guid.NewGuid().ToString();
+        _name = name;
+        _description = description;
+    }
+
+    public static Role Create(string name, string? description = null)
+    {
+        return new Role(name, description);
+    }
+
+    public void SetName(string? name)
+    {
+        _name = name;
+    }
+}
---
+++ Identity.Domain/Roles/RoleClaim.cs
@@ -0,0 +1,35 @@
+﻿namespace Identity.Domain.Roles;
+
+public sealed class RoleClaim : Entity
+{
+    private string _id = default!;
+    private string _roleId = default!;
+    private string? _type;
+    private string? _value;
+
+    public string Id => _id;
+
+    public string RoleId => _roleId;
+
+    public string? Type => _type;
+
+    public string? Value => _value;
+
+    public Claim ToClaim() =>
+        new (Type!, Value!);
+
+    public void InitializeFromClaim(Claim? other)
+    {
+        _type = other?.Type;
+        _value = other?.Value;
+    }
+
+    private RoleClaim() { }
+
+    public RoleClaim(string roleId, string type, string value)
+    {
+        _roleId = roleId;
+        _type = type;
+        _value = value;
+    }
+}
