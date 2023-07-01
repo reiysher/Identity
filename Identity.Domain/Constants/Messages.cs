fb380c32aa4c42e3ae80895367ba4c2c5233c657
Reiysher <reiysher25@gmail.com>
2023-07-01 22:48:23 +0300

    feat(exceptions): add exceptions messages

---
+++ Identity.Domain/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿namespace Identity.Domain.Constants;
+
+public static class Messages
+{
+    public static class Exceptions
+    {
+        public const string InternalServerError = "internal_server_error";
+        public const string AuthenticationFailed = "authentication_failed";
+        public const string InvalidRefreshToken = "invalid_refresh_token";
+        public const string InvalidToken = "invalid_token";
+        public const string InvalidEmail = "invalid_email";
+        public const string InvalidCountryCode = "invalid_country_code";
+        public const string InvalidPhoneNumber = "invalid_phone_number";
+        public const string InvalidRoleName = "invalid_role_name";
+    }
+}
